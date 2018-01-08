8d863f8333113872ba32fbcd8aa6d355ccc492b2
Tomiwa Oyebamiji <TomiwaOyebamiji@gmail.com>
2018-01-08 11:12:31 +0000

    Added way to pick up changes from file, and save them to array,

--- SpellingBee/Spelling_Test.vb
+++ SpellingBee/Spelling_Test.vb
@@ -1,29 +1,48 @@
-﻿Public Class Spelling_Test
-	Dim answer(100) As String
-	Dim Definition(100) As String
+﻿Imports System.IO
+
+Public Class Spelling_Test
+    Dim answer(100) As String
+    Dim Definition(100) As String
     Dim c As Integer = 0
     Dim x As Integer = 0
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles cmd_start.Click
-		cmd_start.Text = "Continue"
-		lbl_Question.Text = "Question " & c
-		Dim fullline As String = ""
-		Dim answer As String = ""
-		Dim time As String
-		FileOpen(1, "n:\Computing\Year 2\Spelling Bee\SpellingBee\SpellingBee\bin\Debug\Spelling_Create.csv", OpenMode.Input)
-		Do Until EOF(1)
-			fullline = LineInput(1)
-			Dim item() As String = Split(fullline, ",")
-            Definition(x) = item(0)
-            time = item(3)
-			x += 1
-		Loop
-		FileClose(1)
-		txt_Defination.Text = Definition(c)
-		c += 1
-	End Sub
+    Private DefWords As New List(Of String())
+    Private DefWords_SIZE As Integer
 
-	Private Sub Spelling_Test_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Public Sub RefreshDefWords()
+        Dim indexLoop1 As Integer = 0
+        Dim fileToCheck As New FileInfo("n:\Computing\Year 2\Spelling Bee\SpellingBee\SpellingBee\bin\Debug\Spelling_Create.csv")
+
+        If (fileToCheck.Length <> DefWords_SIZE) Then
+            FileOpen(1, "n:\Computing\Year 2\Spelling Bee\SpellingBee\SpellingBee\bin\Debug\Spelling_Create.csv", OpenMode.Input)
+            Do Until EOF(1)
+
+                'DefWords = Split(LineInput(1), ",") 'This will read the entire line and store 
+
+                DefWords.Add(Split(LineInput(1), ",")) 'This will read the entire line and store 
+
+                indexLoop1 += 1
+            Loop
+            DefWords_SIZE = fileToCheck.Length
+            FileClose(1)
+        End If
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles cmd_start.Click
+        cmd_start.Text = "Continue"
+        lbl_Question.Text = "Question " & c
+        Dim fullline As String = ""
+        Dim answer As String = ""
+        Dim time As String
+        RefreshDefWords()
+        txt_Defination.Text = Definition(c)
+        c += 1
+    End Sub
+
+    Private Sub Spelling_Test_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
@@ -36,14 +55,7 @@
         Dim words As String
         Dim ThreeLetters As String = ""
         Dim ThreeInputs As String = ""
-        FileOpen(1, "n:\Computing\Year 2\Spelling Bee\SpellingBee\SpellingBee\bin\Debug\Spelling_Create.csv", OpenMode.Input)
-		Do Until EOF(1)
-            fullline = LineInput(1)
-			Dim item() As String = Split(fullline, ",")
-            answer(x) = item(1)
-            x += 1
-		Loop
-        FileClose(1)
+        RefreshDefWords()
 
         Dim letters() = answer(c).ToCharArray()
         words = txt_answer.Text
@@ -60,34 +72,34 @@
             MsgBox("3 Marks")
         Else
             MsgBox("incorrect")
-		End If
-		If txt_answer.Text = answer(c) Then
-			MsgBox("5 Marks")
-		Else
-			MsgBox("3 Marks")
-		End If
-		c += 1
-		Clear()
-	End Sub
+        End If
+        If txt_answer.Text = answer(c) Then
+            MsgBox("5 Marks")
+        Else
+            MsgBox("3 Marks")
+        End If
+        c += 1
+        Clear()
+    End Sub
     Private Sub cmd_answer_TextChanged(sender As Object, e As EventArgs) Handles txt_answer.TextChanged
 
     End Sub
 
-	Private Sub TextBox1_TextChanged_1(sender As Object, e As EventArgs) Handles txt_DateSet.TextChanged
-		Dim fullline As String = ""
-		Dim time As String
-		FileOpen(1, "n:\Computing\Year 2\Spelling Bee\SpellingBee\SpellingBee\bin\Debug\Spelling_Create.csv", OpenMode.Input)
-		Do Until EOF(1)
-			fullline = LineInput(1)
-			Dim item() As String = Split(fullline, ",")
-			txt_Defination.Text = item(0)
-			time = item(3)
+    Private Sub TextBox1_TextChanged_1(sender As Object, e As EventArgs) Handles txt_DateSet.TextChanged
+        Dim fullline As String = ""
+        Dim time As String
+        FileOpen(1, "n:\Computing\Year 2\Spelling Bee\SpellingBee\SpellingBee\bin\Debug\Spelling_Create.csv", OpenMode.Input)
+        Do Until EOF(1)
+            fullline = LineInput(1)
+            Dim item() As String = Split(fullline, ",")
+            txt_Defination.Text = item(0)
+            time = item(3)
 
-		Loop
-		FileClose(1)
-		time = txt_DateSet.Text
-	End Sub
-	Sub Clear()
-		txt_answer.Text = ""
-	End Sub
+        Loop
+        FileClose(1)
+        time = txt_DateSet.Text
+    End Sub
+    Sub Clear()
+        txt_answer.Text = ""
+    End Sub
 End Class
